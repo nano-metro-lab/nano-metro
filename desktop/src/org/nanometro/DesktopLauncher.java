b4439532a1b7ae4becaaab19df38eddd3dd5ee6e
exuanbo <exuanbo@protonmail.com>
2023-01-18 21:30:03 +0000

    initial commit

---
+++ desktop/src/org/nanometro/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package org.nanometro;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import org.nanometro.NanoMetro;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Nano Metro");
+		new Lwjgl3Application(new NanoMetro(), config);
+	}
+}
