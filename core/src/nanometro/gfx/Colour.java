dd8e58b6208f8748c27328710fa7077f82bdd0d9
Jin Yijie <jinyijie@outlook.com>
2023-03-02 17:58:12 +0000

    Bug fix.

--- core/src/nanometro/gfx/Colour.java
+++ core/src/nanometro/gfx/Colour.java
@@ -9,21 +9,23 @@ public class Colour {
     public String subColour1, subColour2, subColour3;
     public boolean occupied = false;
     private static final float ratio2 = 0.5f;
-    private static final float ratio3 = 0.5f;
+    private static final float ratio3 = 0.8f;
     public Colour(String colourHex) {
         this.subColour1 = colourHex;
-        // Todo
-        int r = Integer.parseInt(colourHex.substring(1, 3), 16);
-        int g = Integer.parseInt(colourHex.substring(3, 5), 16);
-        int b = Integer.parseInt(colourHex.substring(5, 7), 16);
+        int r, g, b;
+        r = Integer.parseInt(colourHex.substring(1, 3), 16);
+        g = Integer.parseInt(colourHex.substring(3, 5), 16);
+        b = Integer.parseInt(colourHex.substring(5, 7), 16);
         r = (int) (r * ratio2);
         g = (int) (g * ratio2);
         b = (int) (b * ratio2);
-        // Todo End
         this.subColour2 = String.format("#%02x%02x%02x", r, g, b);
-        r = (int) ((255-r) * ratio3)+r;
-        g = (int) ((255-g) * ratio3)+g;
-        b = (int) ((255-b) * ratio3)+b;
+        r = Integer.parseInt(colourHex.substring(1, 3), 16);
+        g = Integer.parseInt(colourHex.substring(3, 5), 16);
+        b = Integer.parseInt(colourHex.substring(5, 7), 16);
+        r = (int) (255 - (255-r) * ratio3);
+        g = (int) (255 - (255-g) * ratio3);
+        b = (int) (255 - (255-b) * ratio3);
         this.subColour3 = String.format("#%02x%02x%02x", r, g, b);
         colourList.add(this);
     }
