bda09c8947d62f61e58840fe4498ff37a41177f8
Jin Yijie <jinyijie@outlook.com>
2023-03-02 12:19:37 +0000

    Colour update

--- core/src/nanometro/gfx/Colour.java
+++ core/src/nanometro/gfx/Colour.java
@@ -8,10 +8,14 @@ public class Colour {
     static List<Colour> colourList = GameScreen.colourList;
     public String subColour1, subColour2, subColour3;
     public boolean occupied = false;
+    private static float ratio2 = 0.5f;
+    private static float ratio3 = 0.5f;
     public Colour(String colourHex) {
         this.subColour1 = colourHex;
+        // Todo
         this.subColour2 = colourHex;
         this.subColour3 = colourHex;
+        // Todo End
         colourList.add(this);
     }
 
